6e685d5bff2aa6994ae6610dc2acc5087e75294d
Edmilson Monteiro Rodrigues Neto <e.rodrigues@roborian.com>
2024-08-29 20:12:06 +0100

    created tests for TDD

--- simple_task.py
+++ simple_task.py
@@ -1,71 +1,79 @@
+from datetime import datetime
 from sys import argv
 from json import dump, load
 import os
+from typing import Callable
 
 
-FUNCTIONS = {"add", "update", "delete", "mark-in-progress", "mark-done", "mark-todo", "list"}
+FUNCTIONS = {
+    "add",
+    "update",
+    "delete",
+    "mark-in-progress",
+    "mark-done",
+    "mark-todo",
+    "list",
+}
 JSON_PATH = os.path.join(os.getcwd(), "tasks.json")
 
 if not os.path.exists(JSON_PATH):
     dump([], open(JSON_PATH, "w"))
 
 tasks = load(open(JSON_PATH))
-     
+
 
 class WrongArguments(Exception):
     pass
-    
+
 
 class WrongFunctionArguments(Exception):
     pass
 
 
-def argparser() -> dict:
+def argparser() -> tuple[str, list]:
     if len(argv) < 2:
         raise WrongArguments
-        
+
     func = argv[1]
-    
+
     if func not in FUNCTIONS:
         raise WrongArguments
-        
-    func = func.replace("-", "_")    
-        
-    arguments = arg[2:]
-    
+
+    func = func.replace("-", "_")
+
+    arguments = argv[2:]
+
     return func, arguments
-    
-    
+
+
 def add(description: str) -> None:
-    if not isinstance(description, str):
-         raise WrongFunctionArguments
     if len(tasks) >= 1:
-         id = tasks[-1]["id"] + 1
+        id = tasks[-1]["id"] + 1
     else:
-         id = 1
+        id = 1
     tasks.append(
-                     {
-                           "id": id,
-                           "status": "todo",
-                           "description": description,
-                           "created_at": datetime.now(),
-                           "updated_at": datetime.now()
-                     }
+        {
+            "id": id,
+            "status": "todo",
+            "description": description,
+            "created_at": datetime.now(),
+            "updated_at": datetime.now(),
+        }
     )
 
 
+def main():
+    func, arguments = argparser()
+    function = globals()[func]
+
+    try:
+        function(arguments)
+    except Exception:
+        raise WrongFunctionArguments(function)
 
 
-    
- def main():
-     func, arguments = argparser()
-     function = globals()[func]
-     
-     try:
-         function(arguments)
-     except Exception:
-         raise WrongFunctionArguments(function)
-        
-        
-        
-    
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        dump(tasks, open(JSON_PATH, "w"))
---
+++ tests.py
@@ -0,0 +1,61 @@
+import simple_task
+
+
+def test_add():
+    simple_task.add("test")
+    assert simple_task.tasks[-1]["description"] == "test"
+
+
+def test_update():
+    simple_task.add("test")
+    simple_task.update(1, "updated")
+    assert simple_task.tasks[-1]["description"] == "updated"
+
+
+def test_mark_in_progress():
+    simple_task.mark_in_progress(1)
+    assert simple_task.tasks[-1]["status"] == "in-progress"
+
+
+def test_mark_done():
+    simple_task.mark_done(1)
+    assert simple_task.tasks[-1]["status"] == "done"
+
+
+def test_list():
+    assert simple_task.list() == simple_task.tasks
+
+
+def test_list_mark_todo():
+    simple_task.mark_todo(1)
+    assert simple_task.list() == simple_task.tasks
+
+
+def test_delete():
+    simple_task.delete(1)
+    assert not simple_task.tasks
+
+
+def test_argparser():
+    simple_task.argv = ["simple_task.py", "add", "test"]
+    assert simple_task.argparser() == ("add", ["test"])
+
+
+def test_argparser_wrong_arguments():
+    simple_task.argv = ["simple_task.py"]
+    try:
+        simple_task.argparser()
+    except simple_task.WrongArguments:
+        assert True
+    else:
+        assert False
+
+
+def test_argparser_wrong_function_arguments():
+    simple_task.argv = ["simple_task.py", "add", "test", "wrong"]
+    try:
+        simple_task.argparser()
+    except simple_task.WrongArguments:
+        assert True
+    else:
+        assert False
